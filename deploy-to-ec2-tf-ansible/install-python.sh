4778e8f64f5f313c09936505eb7e09e4794c4283
TsembA <tsembenhoi@gmail.com>
2025-06-07 21:39:07 -0700

    deploy to ec2

--- deploy-to-ec2-tf-ansible/install-python.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-yum update -y
-amazon-linux-extras enable python3.8
-yum install -y python38
-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
-alternatives --set python3 /usr/bin/python3.8
